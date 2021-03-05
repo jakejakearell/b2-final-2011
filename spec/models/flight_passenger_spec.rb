81f1e821568794e15356925e8eaef792c0787f98
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-05 09:30:12 -0700

    Change assertion to singular

--- spec/models/flight_passenger_spec.rb
+++ spec/models/flight_passenger_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe FlightPassenger, type: :model do
   describe "relationships" do
     it {should belong_to :flight}
-    it {should belong_to :passengers}
+    it {should belong_to :passenger}
   end
 end
