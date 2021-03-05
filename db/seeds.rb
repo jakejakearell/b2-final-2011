4377c00b9499af9071f82a985ddbfd8509139708
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-05 10:43:43 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+flight_1 = Flight.create!(number: 123, time: 0700, departure_city:"Las Vegas", arrival_city: "Denver", date: '2000-01-01 00:00:00 -0500')
+flight_2 = Flight.create!(number: 124, time: 0700, departure_city:"Las Vegas", arrival_city: "Albany", date: '2000-01-01 00:00:00 -0500')
+flight_3 = Flight.create!(number: 125, time: 0700, departure_city:"Las Vegas", arrival_city: "Zurich", date: '2000-01-01 00:00:00 -0500')
+
+seth = Passenger.create!(name: "Seth", age: 20)
+jeff = Passenger.create!(name: "Jeff", age: 30)
+jake = Passenger.create!(name: "Jake", age: 1)
+
+item_1 = FlightPassenger.create!(flight: flight_1, passenger: seth)
+item_2 = FlightPassenger.create!(flight: flight_1, passenger: jeff)
+item_3 = FlightPassenger.create!(flight: flight_1, passenger: jake)
