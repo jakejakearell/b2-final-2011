cfab295ed18cd9cb88b21b720ed532b777a5ffce
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-05 10:33:14 -0700

    Write model and view code to pass test

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,6 +7,11 @@ class Flight <ApplicationRecord
     .where("age >= 18")
   end
 
+  def average_age
+    adult_passengers
+    .average(:age)
+  end
+
   def self.flights_alphabetical_by_arrival_city
     Flight.order(:arrival_city)
   end
