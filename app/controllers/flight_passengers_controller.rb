564af655f682c9d73ff5c41ddc62e66ff3df0144
Jacob Arellano <jmm.arellano@gmail.com>
2021-03-05 11:35:38 -0700

    Implement the find_id method for FLightPassengers

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,7 +1,7 @@
 class FlightPassengersController < ApplicationController
 
   def destroy
-    flight_passenger_id = FlightPassenger.where("passenger_id = ? AND flight_id = ?", params[:passenger_id], params[:id]).pluck(:id)
+    flight_passenger_id = FlightPassenger.find_id(params[:passenger_id], params[:id])
     FlightPassenger.destroy(flight_passenger_id)
 
     redirect_to flight_path(params[:id])
--- app/models/flight_passenger.rb
+++ app/models/flight_passenger.rb
@@ -1,4 +1,9 @@
 class FlightPassenger < ApplicationRecord
   belongs_to :flight
   belongs_to :passenger
+
+  def self.find_id(passenger_id, flight_id)
+    where("passenger_id = ? AND flight_id = ?", passenger_id, flight_id)
+    .pluck(:id)
+  end
 end
